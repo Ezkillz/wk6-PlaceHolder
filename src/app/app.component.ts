0c988bf48374ae5910a90f62b6883ab74e2559d9
Zeke Gomez <[zzekegomez09@gmail.com]>
2020-02-11 18:52:58 -0600

    really need to type and code as fast as he does!!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,12 @@ import { PlaceholderService } from '../Services/placeholder.service'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private placeholder: )
+  constructor(private placeholder: PlaceholderService ){}
+    displayedColumns: string[] = ['userId','id','title','completed']
   title = 'Placeholder';
+  dataSource;
+
+  mgOnInit(): Promise<void>{
+    this.dataSource = await this.placeholder.get();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
+import {MatTableModule } from '@angular/'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
